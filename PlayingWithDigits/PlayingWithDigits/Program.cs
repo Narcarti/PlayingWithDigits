6e1b9eb34a8c9b0e6035d590a61075fc88e68c41
Narcarti <tcmarkymark@gmail.com>
2019-10-29 15:05:59 -0500

    Add files via upload

---
+++ PlayingWithDigits/PlayingWithDigits/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(digPow(89, 1));
+            Console.WriteLine(digPow(695, 2));
+            Console.WriteLine(digPow(46288, 3));
+
+            Console.ReadKey();
+        }
+
+
+        public static long digPow(int n, int p)
+        {
+            string nums = n.ToString();
+            char[] numA = nums.ToCharArray();
+            int powCnt = p;
+            double dig = 0;
+            for (int idx = 0; idx < numA.Length; idx++)
+            {
+                dig += Math.Pow(int.Parse(numA[idx].ToString()), p+idx);
+                powCnt++;
+            }
+            Console.WriteLine(dig);
+            Console.WriteLine(dig /n);
+            Console.WriteLine();
+
+            if (dig % n == 0)
+            {
+                return (long)dig / n;
+            }
+            return ((dig == n || (dig == n * p)) ? p : -1 );
+        }
+    }
+}
